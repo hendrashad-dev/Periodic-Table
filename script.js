bcbda3ba7a159fe422018c29e1aba2b4a6c1430c
hend5rashad <hend5rashad@gmail.com>
2025-12-20 14:00:46 +0000

    added search

--- script.js
+++ script.js
@@ -23,6 +23,14 @@ const elements = [
         columon: 15,
         row: 2
     },
+    {
+        numberAtoms: 5, 
+        name: "example",
+        symbol: "E",
+        categoryInTable: "nonmetal",
+        columon: 32,
+        row: 2
+    },
     
 ]
 
@@ -50,3 +58,38 @@ function createPeriodicTable(elements) {
 }
 
 createPeriodicTable(elements)
+
+
+const searchInput = document.getElementById('searchInput')
+
+searchInput.addEventListener('input', (e) => {
+    const query = e.target.value.toLowerCase()
+
+
+    if (query === '') {
+
+        document.querySelectorAll('.element-card').forEach(cardElement =>  {
+             cardElement.style.opacity = '1'
+            })
+    
+    } else {
+
+        document.querySelectorAll('.element-card').forEach(card => {
+
+            const name = card.querySelector('.element-name').textContent.toLowerCase()
+            const symbol = card.querySelector('.element-symbol').textContent.toLowerCase()
+            const num = card.querySelector('.element-number').textContent
+
+            if (name.includes(query) || symbol.includes(query) || num.includes(query)) {
+                card.style.opacity = '1'
+                card.style.transform = 'scale(1.1)'
+                card.style.zIndex = '5'
+            } else {
+                card.style.opacity = '0.1'
+                card.style.transform = 'scale(1)'
+                card.style.zIndex = '1'
+            }
+            
+        });
+    }
+});
