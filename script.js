366aa855a889b5ae045289332b706d3b81a96359
hend5rashad <hend5rashad@gmail.com>
2025-12-20 10:41:11 +0000

    Added createPeriodicTable()

--- script.js
+++ script.js
@@ -3,7 +3,29 @@ const elements = [
         numberAtoms: 1, 
         name: "Hydrogen",
         symbol: "H",
-        categoryInTable: "nonmetal"
+        categoryInTable: "nonmetal",
+        columon: 1,
+        row: 1
     },
     
 ]
+
+const periodicTable = document.getElementById("periodic-table")
+
+function createPeriodicTable(elements) {
+
+    elements.forEach(element => {
+        let cardElement = document.createElement('div');
+        cardElement.className = "element-card" + " cat-" + element.categoryInTable
+
+        cardElement.innerHTML = `
+        <span class="element-number">${element.numberAtoms}</span>
+        <span class="element-symbol">${element.symbol}</span>
+        <span class="element-name">${element.name}</span>
+        `;
+
+        periodicTable.appendChild(cardElement)
+    });
+}
+
+createPeriodicTable(elements)
